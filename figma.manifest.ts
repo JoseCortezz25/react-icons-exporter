65bff91a2afa7c534490332cf85aa5fb7b9f6870
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2026-03-23 12:24:29 -0500

    update manfiest

--- figma.manifest.ts
+++ figma.manifest.ts
@@ -1,11 +1,15 @@
 // https://www.figma.com/plugin-docs/manifest/
 export default {
   name: 'React Icons Exporter',
-  id: '1222852692367737510',
+  id: '1612159383801445987',
   api: '1.0.0',
   main: 'plugin.js',
   ui: 'index.html',
   capabilities: [],
   enableProposedApi: false,
-  editorType: ['figma', 'figjam']
+  editorType: ['figma'],
+  networkAccess: {
+    allowedDomains: ['none']
+  },
+  documentAccess: 'dynamic-page'
 };
